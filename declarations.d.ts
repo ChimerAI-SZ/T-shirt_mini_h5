8b5d60e4518cd69c890c204765aa1c738cf55a25
lingzhi <13714733197@163.com>
2024-12-02 09:29:10 +0800

    印花上身开发

--- declarations.d.ts
+++ declarations.d.ts
@@ -1,2 +1,13 @@
 declare module "prop-types";
 declare module "lucide-react";
+declare module 'react-moveable' {
+    const Moveable: any;
+    export default Moveable;
+}
+declare module 'react-draggable' {
+    const Draggable: any;
+    export default Draggable;
+}
+declare module 'react-resizable' {
+    export const ResizableBox: any;
+}
