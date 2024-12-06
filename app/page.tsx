5238072e1e59a3fbfdbacc55414ee42428a7865a
lz <13714733197@163.com>
2024-12-06 13:22:51 +0800

    修改

--- app/page.tsx
+++ app/page.tsx
@@ -299,8 +299,8 @@ function Dashboard() {
       Alert.open({
         content: "当前wx11"
       })
-      // wx.miniProgram.navigateTo({ url: "/pages/addtocart/index" })
-      wx.miniProgram.navigateBack({ delta: 1 })
+      wx.miniProgram.navigateTo({ url: "/pages/pages/addtocart/index" })
+      // wx.miniProgram.navigateBack({ delta: 1 })
     }
 
     const navigateToMiniProgram = () => {
